1cc745de2bbda509c115a31aebbeba88d9c06653
Jonas Ellington <jonas@Jonass-Laptop.local>
2023-11-04 15:41:15 -0500

    initial commit of spring application

---
+++ backend/src/main/java/com/example/HackUTDX/HackUtdxApplication.java
@@ -0,0 +1,13 @@
+package com.example.HackUTDX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackUtdxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackUtdxApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/HackUTDX/HackUtdxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HackUTDX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackUtdxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
